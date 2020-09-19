d2b57d6a6728fb195d5677b03dbf2606ca9ebed4
Yabacoder <yabacoder@gmail.com>
2020-09-19 02:19:57 +0100

    center sorted

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {        
+        softblue: '#7D8DE3',
+        grayblue: '#9194A1',
+        darkblue: '#252B46',
+        bkred: '#FA5757'
+      },
+    },
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus','active']
+  },
+  plugins: [],
+}
